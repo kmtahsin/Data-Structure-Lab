e3c94f49fd77b076ab824f0a27e7cb90a45fe8f6
kmtahsin <113965329+kmtahsin@users.noreply.github.com>
2022-09-27 12:58:49 +0600

    Add files via upload

---
+++ Problem 1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=5,j=8,b=0,c=0;
+    int array_1[i]={10,20,30,40,50};
+    int array_2[j]={1,2,3,4,5,6,7,8};
+    int t=i+j;
+    int marge_array[t];
+    for(int a=0;a<t;a++)
+    {
+        if(a<i)
+        {
+            marge_array[a]=array_1[b];
+            b++;
+        }
+        else if(a==i || a>i && a<t)
+        {
+            marge_array[a]=array_2[c];
+            c++;
+        }
+    }
+
+    cout<<"Marged array in reverse order: ";
+
+    for(int x=t-1;x>=0;x--)
+    {
+        cout<<marge_array[x]<<"  ";
+    }
+
+    return 0;
+
+}
